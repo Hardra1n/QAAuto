84d4dafdf4886ce80c97780991f2fc48b326ad83
Kiryl Kho Van <k.hovan@yandex.ru>
2021-11-12 23:53:39 +0300

    Integrate InitializationException into Chassis class

--- Model/VehicleComponents/Chassis.cs
+++ Model/VehicleComponents/Chassis.cs
@@ -1,12 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using VehicleXML.Model.Exceptions;
 
 namespace VehicleXML.Model.VehicleComponents
 {
     [Serializable]
     public class Chassis
     {
+        byte _wheelNumber;
+
+        int _maxWeight;
+
+        string _serialNumber;
+
         public Chassis() { }
 
         public Chassis(byte wheelNumber, string serialNumber, int maxWeight)
@@ -15,11 +22,62 @@ namespace VehicleXML.Model.VehicleComponents
             SerialNumber = serialNumber;
             MaxWeight = maxWeight;
         }
-        public byte WheelNumber { get; set; }
+        public byte WheelNumber
+        {
+            get
+            {
+                return _wheelNumber;
+            }
+            set
+            {
+                if (value <= 1)
+                {
+                    throw new InitializationException("Number of wheels must be greater then 1");
+                }
+                _wheelNumber = value;
+            }
+        }
 
-        public string SerialNumber { get; set; }
+        public string SerialNumber
+        {
+            get
+            {
+                return _serialNumber;
+            }
+            set
+            {
+                if (value.Length != 5)
+                {
+                    throw new InitializationException("Chassis serial number must exists only five symbols");
+                }
 
-        public int MaxWeight { get; set; }
+                foreach (char symbol in value)
+                {
+                    if (!Char.IsLetterOrDigit(symbol))
+                    {
+                        throw new InitializationException("Chassis serial number must exists only letters and digits");
+                    }
+                }
+
+                _serialNumber = value;
+            }
+        }
+
+        public int MaxWeight
+        {
+            get
+            {
+                return _maxWeight;
+            }
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new InitializationException("Chassis maximum weight must be greater then 0");
+                }
+                _maxWeight = value;
+            }
+        }
 
 
         /// <summary>
