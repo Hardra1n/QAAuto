9b75692d6faebe86604098ea6c6d72fa273ace54
Kiryl Kho Van <k.hovan@yandex.ru>
2021-11-19 14:29:51 +0300

    Implementation of CountTypesCommand

--- AutoConsoleHandler/Commands/CountTypesCommand.cs
+++ AutoConsoleHandler/Commands/CountTypesCommand.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using AutoConsoleHandler.Model;
+using AutoConsoleHandler.UI;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,9 +8,16 @@ namespace AutoConsoleHandler.Commands
 {
     public class CountTypesCommand : ICommand
     {
+        IWriter _writer;
+
+        public CountTypesCommand(IWriter writer)
+        {
+            _writer = writer;
+        }
         public void Execute()
         {
-            throw new NotImplementedException();
+            CarPark carPark = CarPark.GetInstance();
+            _writer.Write(carPark.CountUniqueTypes());
         }
     }
 }
