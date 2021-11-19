7a02c5f38d39f94a0caf93d1393d51579274ca9c
Kiryl Kho Van <k.hovan@yandex.ru>
2021-11-19 14:38:22 +0300

    Implementation of AveragePriceTypeCommand

--- AutoConsoleHandler/Commands/AveragePriceTypeCommand.cs
+++ AutoConsoleHandler/Commands/AveragePriceTypeCommand.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using AutoConsoleHandler.Model;
+using AutoConsoleHandler.UI;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,9 +8,20 @@ namespace AutoConsoleHandler.Commands
 {
     public class AveragePriceTypeCommand : ICommand
     {
+        IWriter _writer;
+
+        string _type;
+
+        public AveragePriceTypeCommand(IWriter writer, string type)
+        {
+            _type = type;
+            _writer = writer;
+        }
+
         public void Execute()
         {
-            throw new NotImplementedException();
+            CarPark carPark = CarPark.GetInstance();
+            _writer.Write(carPark.CountAveragePriceOfType(_type));
         }
     }
 }
