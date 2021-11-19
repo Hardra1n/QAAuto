9ba51bbfdfe69dfd945766f47f9a82bcbac12476
Kiryl Kho Van <k.hovan@yandex.ru>
2021-11-19 12:41:57 +0300

    Add UI not implemented model

---
+++ AutoConsoleHandler/UI/ConsoleHandler.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AutoConsoleHandler.UI
+{
+    public class ConsoleHandler : IWriter
+    {
+        public void Write<T>(T data)
+        {
+            Console.Write(data);
+        }
+    }
+}
---
+++ AutoConsoleHandler/UI/IWriter.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AutoConsoleHandler.UI
+{
+    interface IWriter
+    {
+        void Write<T>(T data);
+    }
+}
