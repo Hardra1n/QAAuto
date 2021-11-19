c423a6b800a174343e90e70ceadb2760a741aecf
Kiryl Kho Van <k.hovan@yandex.ru>
2021-11-19 14:59:34 +0300

    Change add items into CarPark from Command to Adder

---
+++ AutoConsoleHandler/Adders/CarParkAdder.cs
@@ -0,0 +1,18 @@
+﻿using AutoConsoleHandler.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AutoConsoleHandler.Adders
+{
+    public class CarParkAdder : ICarParkAdder
+    {
+        public CarParkAdder() { }
+
+        public void AddCarGroup(string type, string model, float price, int amount)
+        {
+            CarPark carPark = CarPark.GetInstance();
+            carPark.AddCarGroup(type, model, price, amount);
+        }
+    }
+}
---
+++ AutoConsoleHandler/Adders/ICarParkAdder.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AutoConsoleHandler.Adders
+{
+    interface ICarParkAdder
+    {
+        public void AddCarGroup(string type, string model, float price, int amount);
+    }
+}
