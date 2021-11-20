1ea11a3c05d2a4cc51055ef9c9d7a1d12d304513
Kiryl Kho Van <k.hovan@yandex.ru>
2021-11-20 23:59:14 +0300

    Implement ConsoleHandler work without working ExitCommand

--- AutoConsoleHandler/UI/ConsoleHandler.cs
+++ AutoConsoleHandler/UI/ConsoleHandler.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using AutoConsoleHandler.Adders;
+using AutoConsoleHandler.Commands;
+using AutoConsoleHandler.Model;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,9 +9,55 @@ namespace AutoConsoleHandler.UI
 {
     public class ConsoleHandler : IWriter
     {
+        Dictionary<string, ICommand> commands = new Dictionary<string, ICommand>();
+
+        public ConsoleHandler()
+        {
+            CommandInit();
+        }
+
+        //TODO Implement 'exit command'
+        public void Start()
+        {
+            while (true)
+            {
+                InterpitateCommand(ReadCommand());
+            }
+        }
+        
+        private void CommandInit()
+        {
+            commands.Add("average price", new AveragePriceCommand(this));
+            commands.Add("count all", new CountAllCommand(this));
+            commands.Add("count types", new CountTypesCommand(this));
+            commands.Add("exit", new ExitCommand());
+            InitAveragePriceTypeCommand();
+        }
+        private void InitAveragePriceTypeCommand()
+        {
+            foreach (string type in CarPark.GetInstance().GetUniqueTypes())
+            {
+                commands.Add("average price " + type, new AveragePriceTypeCommand(this, type));
+            }
+        }
+
+        private string ReadCommand() => Console.ReadLine();
+
+        private void InterpitateCommand(string command)
+        {
+            if (commands.ContainsKey(command))
+            {
+                commands[command].Execute();
+            }
+            else
+            {
+                Console.WriteLine("Unrecognized command");
+            }
+        }
+
         public void Write<T>(T data)
         {
-            Console.Write(data);
+            Console.WriteLine(data);
         }
     }
 }
