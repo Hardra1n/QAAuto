01669646413346a38f1c9d2994a887861e262740
Kiryl Kho Van <k.hovan@yandex.ru>
2021-11-25 15:03:15 +0300

    Add tests to GetUniqueStringMaxLength

---
+++ ConsoleHandlerTests/StringHandlerTests.cs
@@ -0,0 +1,65 @@
+﻿using Console_handler;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace ConsoleHandlerTests
+{
+    [TestClass()]
+    public class StringHandlerTests
+    {
+        [TestMethod()]
+        public void GetUniqueStringMaxLength_ValidString_Gets7()
+        {
+            string str = "asdmwkaadsds";
+            int expected = 7;
+
+            int actual = StringHandler.GetUniqueStringMaxLength(str);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void GetUniqueStringMaxLength_OneSymbol_Gets1()
+        {
+            string str = "$";
+            int expected = 1;
+
+            int actual = StringHandler.GetUniqueStringMaxLength(str);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void GetUniqueMaxLength_EmptyString_Gets0()
+        {
+            string str = string.Empty;
+            int expected = 0;
+
+            int actual = StringHandler.GetUniqueStringMaxLength(str);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void GetUniqueMaxLength_NullString_Gets0()
+        {
+            string str = null;
+            int expected = 0;
+
+            int actual = StringHandler.GetUniqueStringMaxLength(str);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void GetUniqueLength_SequenceOfOneSymbol_Gets1()
+        {
+            string str = "lllllll";
+            int expected = 1;
+
+            int actual = StringHandler.GetUniqueStringMaxLength(str);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
