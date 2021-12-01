928fa3347a6759b4f05d1a2366f15fac3193d6d7
Kiryl Kho Van <k.hovan@yandex.ru>
2021-12-02 01:03:36 +0300

    Init solution and add some XPath strings

---
+++ Locators/Program.cs
@@ -0,0 +1,22 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Edge;
+using System;
+using System.Collections.Generic;
+
+namespace Locators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WebDriver driver = new EdgeDriver();
+            driver.Url = "https://www.bbc.com/sport";
+
+            IWebElement elem = driver.FindElement(By.Id("sp-nav-all-sport-button"));
+            var temp = driver.FindElements(By.XPath("/q"));
+
+
+            driver.Close();
+        }
+    }
+}
