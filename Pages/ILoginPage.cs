26830de2d60ef8122fae418d1d66dfc6c310153c
Kiryl Kho Van <k.hovan@yandex.ru>
2022-01-08 00:31:42 +0300

    Create LoginPage general model

---
+++ Pages/ILoginPage.cs
@@ -0,0 +1,15 @@
+﻿namespace Pages
+{
+    public interface ILoginPage
+    {
+        ILoginPage TypeUsername(string username);
+
+        ILoginPage TypePassword(string password);
+
+        IHomePage SubmitLogin();
+
+        ILoginPage SubmitLoginWithoutSwitchToNewPage();
+
+        IHomePage LoginAs(string username, string password);
+    }
+}
