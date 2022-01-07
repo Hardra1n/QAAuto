8b4f484f17ebdb6deb2e3eafdf1da06c9d398894
Kiryl Kho Van <k.hovan@yandex.ru>
2022-01-08 00:33:09 +0300

    Add base fucntionality to Mail.ru login page

---
+++ Pages/Mailru/MailruLoginPage.cs
@@ -0,0 +1,56 @@
+﻿using OpenQA.Selenium;
+using Pages;
+
+namespace Pages.Mailru
+{
+    public class MailruLoginPage : ILoginPage
+    {
+        private By _usernameInput = By.XPath("//input[@name='username']");
+
+        private By _passwordInput = By.XPath("//input[@name='password']");
+
+        private By _submitLoginButton = By.XPath("//button[@type='submit']");
+
+        public MailruLoginPage(IWebDriver driver)
+        {
+            Driver = driver;
+        }
+
+        IWebDriver Driver { get; set; }
+
+        public ILoginPage TypeUsername(string username)
+        {
+            Waiters.WaitUntilDisplayElement(Driver, _usernameInput);
+            Driver.FindElement(_usernameInput).SendKeys(username);
+            return this;
+        }
+        public ILoginPage TypePassword(string password)
+        {
+            Waiters.WaitUntilDisplayElement(Driver, _passwordInput);
+            Driver.FindElement(_passwordInput).SendKeys(password);
+            return this;
+        }
+
+        public IHomePage SubmitLogin()
+        {
+            Waiters.WaitUntilDisplayElement(Driver, _submitLoginButton);
+            Driver.FindElement(_submitLoginButton).Submit();
+            throw new System.NotImplementedException();
+        }
+
+        public ILoginPage SubmitLoginWithoutSwitchToNewPage()
+        {
+            Waiters.WaitUntilDisplayElement(Driver, _submitLoginButton);
+            Driver.FindElement(_submitLoginButton).Submit();
+            return this;
+        }
+
+        public IHomePage LoginAs(string username, string password)
+        {
+            TypeUsername(username);
+            SubmitLoginWithoutSwitchToNewPage();
+            TypePassword(password);
+            return SubmitLogin();
+        }
+    }
+}
