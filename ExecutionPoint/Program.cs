3b1a130b5d81bf7fd22dc4c78840007d9481ac3c
Kiryl Kho Van <k.hovan@yandex.ru>
2022-01-08 14:44:18 +0300

    Add temp method for test functionality of Yandex.by login page

--- ExecutionPoint/Program.cs
+++ ExecutionPoint/Program.cs
@@ -2,6 +2,7 @@
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Support.UI;
 using Pages.Mailru;
+using Pages.Yandex;
 using System;
 using System.Threading;
 
@@ -11,7 +12,36 @@ namespace ExecutionPoint
     {
         static void Main(string[] args)
         {
-            MailruLoginTest();
+            //MailruLoginTest();
+            YandexLoginTest();
+        }
+
+        static void YandexLoginTest()
+        {
+            string username = "kirylkho123456";
+            string password = "Qwerty1231";
+            WebDriver driver = new ChromeDriver();
+            driver.Manage().Timeouts().ImplicitWait = new TimeSpan(0, 0, 40);
+            try
+            {
+                driver.Url = "https://passport.yandex.by/";
+                Console.WriteLine(driver.Title);
+                YandexLoginPage page = new YandexLoginPage(driver);
+                page.TypeUsername(username);
+                page.SubmitLoginWithoutSwitchToNewPage();
+                page.TypePassword(password);
+                //page.SubmitLogin();
+                Thread.Sleep(10000);
+                Console.WriteLine(driver.Title);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                driver.Close();
+            }
         }
 
         static void MailruLoginTest()
