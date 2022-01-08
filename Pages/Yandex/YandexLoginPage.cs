1aecabc9a574ec28e39152cb246887f982d063de
Kiryl Kho Van <k.hovan@yandex.ru>
2022-01-08 14:43:51 +0300

    Add base functionality to Yandex.by login page

---
+++ Pages/Yandex/YandexLoginPage.cs
@@ -0,0 +1,61 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pages.Yandex
+{
+    public class YandexLoginPage : ILoginPage
+    {
+        private By _usernameInput = By.XPath("//input[@name='login']");
+
+        private By _passwordInput = By.XPath("//input[@name='passwd']");
+
+        private By _submitLogin = By.XPath("//button[@type='submit']");
+
+        public YandexLoginPage(IWebDriver driver)
+        {
+            Driver = driver;
+        }
+
+        private IWebDriver Driver { get; set; }
+
+        public IHomePage LoginAs(string username, string password)
+        {
+            TypeUsername(username);
+            SubmitLoginWithoutSwitchToNewPage();
+            TypePassword(password);
+            return SubmitLogin();
+        }
+
+        public IHomePage SubmitLogin()
+        {
+            Waiters.WaitUntilDisplayElement(Driver, _submitLogin);
+            Driver.FindElement(_submitLogin).Submit();
+            throw new NotImplementedException();
+        }
+
+        public ILoginPage SubmitLoginWithoutSwitchToNewPage()
+        {
+            Waiters.WaitUntilDisplayElement(Driver, _submitLogin);
+            Driver.FindElement(_submitLogin).Submit();
+            return this;
+        }
+
+        public ILoginPage TypePassword(string password)
+        {
+            Waiters.WaitUntilDisplayElement(Driver, _passwordInput);
+            Driver.FindElement(_passwordInput).SendKeys(password);
+            return this;
+        }
+
+        public ILoginPage TypeUsername(string username)
+        {
+            Waiters.WaitUntilDisplayElement(Driver, _usernameInput);
+            Driver.FindElement(_usernameInput).SendKeys(username);
+            return this;
+        }
+    }
+}
