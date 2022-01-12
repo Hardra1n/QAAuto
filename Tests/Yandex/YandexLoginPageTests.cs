44770e58db32c42670be254b7fbfe456d8180c1c
Kiryl Kho Van <k.hovan@yandex.ru>
2022-01-12 23:31:17 +0300

    Create mutable login page tests class

---
+++ Tests/Yandex/YandexLoginPageTests.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using Pages.Interfaces;
+using Pages.Yandex;
+
+namespace Tests.Yandex
+{
+    [TestFixture]
+    public class YandexLoginPageTests
+    {
+        private YandexLoginPage _page;
+
+        private string _correctUsername = "kirylkho123456";
+
+        private string _correctPassword = "Qwerty1231";
+
+        IWebDriver _driver;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _driver = new ChromeDriver();
+            _driver.Url = "https://passport.yandex.by/";
+            _page = new YandexLoginPage(_driver);
+        }
+
+        [TearDown]
+        public void CleanUp()
+        {
+            _driver.Close();
+        }
+
+        [Test]
+        public void LoginWithInccorrectUsername()
+        {
+            throw new System.NotImplementedException();
+        }
+
+    }
+}
