af6b8e795503a9da64ea037ba810076592519be2
Kiryl Kho Van <k.hovan@yandex.ru>
2022-01-18 11:04:06 +0300

    Add base test structure for Mail.ru message composer page

---
+++ Tests/Mailru/MailruMessageComposerTests.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using Pages.Mailru;
+
+namespace Tests.Mailru
+{
+    [TestFixture]
+    public class MailruMessageComposerTests : BaseTest
+    {
+        MailruMessageComposerPage _page;
+
+        IWebDriver _driver;
+
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+            _driver = GetNewChromeDriver();
+            MailruLoginPage loginPage = new MailruLoginPage(_driver);
+
+            _page = (MailruMessageComposerPage)loginPage.LoginAs(AccountCredenitals.mailruLogin,
+                                                         AccountCredenitals.mailruPassword)
+                                                        .GoToMailboxPage();
+        }
+
+        [OneTimeTearDown]
+        public void OneTimeTearDown()
+        {
+            _driver.Close();
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+            _driver.Url = "https://e.mail.ru/inbox";
+            _page = (MailruMessageComposerPage)new MailruMailboxPage(_driver).OpenMessageComposer();
+        }
+    }
+}
