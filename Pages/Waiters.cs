3eb6f99fff2995638a698f45b998d218f5fde508
Kiryl Kho Van <k.hovan@yandex.ru>
2022-02-02 22:08:48 +0300

    Refactoring waiters class

--- Pages/Waiters.cs
+++ Pages/Waiters.cs
@@ -1,6 +1,7 @@
 ﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Support.UI;
 using System;
+using System.Linq;
 
 namespace Pages
 {
@@ -8,69 +9,16 @@ namespace Pages
     {
         private static TimeSpan _waitTimer = new TimeSpan(0, 0, 20);
 
-
-        static private WebDriverWait WaiterInitializer(IWebDriver driver)
-        {
-            WebDriverWait waiter = new WebDriverWait(driver, _waitTimer);
-            waiter.IgnoreExceptionTypes(typeof(NoSuchElementException), 
-                                        typeof(StaleElementReferenceException));
-            return waiter;
-        }
-
-
-        static public void WaitUntilDisplayElement(IWebDriver driver, By locator)
-        {
-            WebDriverWait waiter = WaiterInitializer(driver);
-            waiter.Until(driver =>
-            {
-                IWebElement element = driver.FindElement(locator);
-                if (element.Displayed)
-                {
-                    return true;
-                }
-                return false;
-            });
-        }
+        static public void WaitUntilDisplayElement(IWebDriver driver, By locator) 
+            => new WebDriverWait(driver, _waitTimer).Until(driver => driver.FindElement(locator).Displayed);
 
         static public bool WaitUntilNotVisable(IWebDriver driver, By locator)
-        {
-            WebDriverWait waiter = WaiterInitializer(driver);
-            return waiter.Until(driver =>
-            {
-                IWebElement element = driver.FindElement(locator);
-                if (!element.Displayed)
-                {
-                    return true;
-                }
-                return false;
-            });
-        }
+            => new WebDriverWait(driver, _waitTimer).Until(driver => !driver.FindElement(locator).Displayed);
 
         static public bool WaitUntilTitleEquals(IWebDriver driver, string title)
-        {
-            WebDriverWait waiter = WaiterInitializer(driver);
-            return waiter.Until(driver =>
-            {
-                if (driver.Title == title)
-                {
-                    return true;
-                }
-                return false;
-            });
-        }
+            => new WebDriverWait(driver, _waitTimer).Until(driver => driver.Title == title);
 
-        static public bool WaitUntilTitleContains(IWebDriver driver, string substring)
-        {
-            WebDriverWait waiter = WaiterInitializer(driver);
-            return waiter.Until(driver =>
-            {
-                string driverTitle = driver.Title;
-                if (driverTitle.Contains(substring))
-                {
-                    return true;
-                }
-                return false;
-            });
-        }
+        static public bool WaitUntilTitleContains(IWebDriver driver, string substring) 
+            => new WebDriverWait(driver, _waitTimer).Until(driver => driver.Title.Contains(substring));
     }
 }
